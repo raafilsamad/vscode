abb0d74de2d26d5bd76f417f349d196e0d6ea7a1
unknown <raafilsamad122004@gmail.com>
2025-07-12 16:53:51 +0530

    logic for odd or even

---
+++ mark.py
@@ -0,0 +1,7 @@
+score=int(input("enter your score within 100:"))
+if(score<35):
+    print("Poor student")
+if(score>35 and score<70):
+    print("average student")
+if(score>70):
+    print("Good student")
